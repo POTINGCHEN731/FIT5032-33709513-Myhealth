0199037e0dd4d534ea1bf97499d6e7a1e61ba449
POTINGCHEN731 <163748756+POTINGCHEN731@users.noreply.github.com>
2024-10-18 00:14:23 +1100

    Update finction

--- 33709513/functions/index.js
+++ 33709513/functions/index.js
@@ -11,22 +11,22 @@ const {onRequest} = require("firebase-functions/v2/https");
 const sgMail = require("@sendgrid/mail");
 const admin = require("firebase-admin");
 const cors = require("cors")({origin: true});
+const functions = require("firebase-functions");
 
 admin.initializeApp();
 const db = admin.firestore();
 
 sgMail.setApiKey(
-    "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");
+    "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");
 
 exports.sendEmail = onRequest(async (req, res) => {
   cors(req, res, async () => {
     if (req.method !== "POST") {
       return res.status(405).send("Method Not Allowed");
     }
-
     const msg = {
       to: "terence07312000@gmail.com",
-      from: "felix07312000@gmail.com",
+      from: "myhealth0731@gmail.com",
       subject: "Sending with SendGrid is Fun",
       text: "and easy to do anywhere, even with Node.js",
       html: "<strong>and easy to do anywhere, even with Node.js</strong>",
@@ -34,7 +34,6 @@ exports.sendEmail = onRequest(async (req, res) => {
 
     try {
       await sgMail.send(msg);
-      res.set("Access-Control-Allow-Origin", "*");
       res.status(200).send("Email sent successfully!");
     } catch (error) {
       console.error(error);
@@ -42,6 +41,28 @@ exports.sendEmail = onRequest(async (req, res) => {
     }
   });
 });
+exports.sendEmailOnDelete = functions.firestore
+    .document("Appointment/{docId}")
+    .onDelete(async (snap, context) => {
+      const deletedValue = snap.data();
+
+      const msg = {
+        to: "terence07312000@gmail.com",
+        from: "medlingobookings@gmail.com",
+        subject: "Firestore Document Deleted",
+        text: `A document was deleted from Firestore. 
+        Details: ${JSON.stringify(deletedValue)}`,
+        html: `<strong>A document was deleted from Firestore.</strong><br/>
+        Details: ${JSON.stringify(deletedValue)}`,
+      };
+
+      try {
+        await sgMail.send(msg);
+        console.log("Email sent successfully");
+      } catch (error) {
+        console.error("Error sending email:", error);
+      }
+    });
 
 exports.AverageRating = onRequest(async (req, res) => {
   cors(req, res, async () => {
